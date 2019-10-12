3729b8a840258f9ab3431ebda3bd167330bed8db
sirdude103 <sirdude103@gmail.com>
2019-10-12 01:43:41 -0500

    Successfully pushed queries to the database and preparing additional side files

--- main.py
+++ main.py
@@ -14,3 +14,32 @@ conn = psycopg2.connect(database=url.path[1:],
                         password=url.password,
                         host=url.hostname,
                         port=url.port)
+
+cur = conn.cursor()
+
+cur.execute("""DROP TABLE IF EXISTS characters""")
+cur.execute("""CREATE TABLE IF NOT EXISTS characters (
+                name TEXT,
+                race TEXT,
+                class TEXT,
+                level INTEGER
+                )""")
+
+initial_character = {
+    "name": "John Smith",
+    "race": "Elf",
+    "class": "Barbarian",
+    "level": 1
+}
+
+cur.execute("""INSERT INTO characters VALUES ( %(name)s, %(race)s, %(class)s, %(level)s)""", initial_character)
+
+cur.execute("""SELECT * FROM characters""")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+conn.commit()
+
+cur.close()
+conn.close()
